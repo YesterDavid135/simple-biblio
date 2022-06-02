10e28bfb5a4a2f46a51624522ed5075601b1eaca
ydavid <hello@ydavid.ch>
2022-06-02 14:39:02 +0200

    Added DTO's

---
+++ src/dto/Location.java
@@ -0,0 +1,39 @@
+package dto;
+
+
+public class Location {
+
+    private int idLoc;
+    private String ZIP;
+    private String location;
+
+    public Location(int idLoc, String ZIP, String location) {
+        this.idLoc = idLoc;
+        this.ZIP = ZIP;
+        this.location = location;
+    }
+
+    public int getIdLoc() {
+        return idLoc;
+    }
+
+    public void setIdLoc(int idLoc) {
+        this.idLoc = idLoc;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ src/dto/MediaType.java
@@ -0,0 +1,7 @@
+package dto;
+
+public enum MediaType {
+
+    BUCH, DVD, AUDIOCD, COMPUTERGAMES
+
+}
